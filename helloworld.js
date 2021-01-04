6e579b8aa5165c5ff9f395cd41b1c6cfe67e0dd2
MeghanaPvs <pvsmeghanachowdary@gmail.com>
2021-01-04 12:31:26 +0530

    Hello world in node js

---
+++ helloworld.js
@@ -0,0 +1,17 @@
+const express= require('express');//importing express
+var app=express(); //Calling express function ; where app is an object 
+
+let sample={
+    name:"Meghana",
+    age:"21"
+}
+
+app.get('/',(req,res)=>{
+    res.send(sample);
+})
+
+
+app.listen(3000,function(){
+    console.log('APP listening on Port 3000')
+})
+//liten on port no. 3000
